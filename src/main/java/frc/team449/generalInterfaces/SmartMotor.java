f932f92141292fdc4f7fcd43cf843bfd5dfbe5ac
KiARC Repository Manager CF <>
2021-12-10 21:17:13 +0000

    Automated Code Formatting
    Using the KiARC Repository Manager

--- src/main/java/frc/team449/generalInterfaces/SmartMotor.java
+++ src/main/java/frc/team449/generalInterfaces/SmartMotor.java
@@ -89,8 +89,8 @@ public interface SmartMotor extends SimpleMotor, Shiftable, Loggable {
   void setPositionSetpoint(double meters);
 
   /**
-   * Since the PID controller isn't accessible to this interface, this method must be
-   * overwritten to set values for P, I, and D in {@link MappedSparkMaxBase#setGear(int gear)}
+   * Since the PID controller isn't accessible to this interface, this method must be overwritten to
+   * set values for P, I, and D in {@link MappedSparkMaxBase#setGear(int gear)}
    */
   void setPID(double kP, double kI, double kD);
 
@@ -272,7 +272,8 @@ public interface SmartMotor extends SimpleMotor, Shiftable, Loggable {
       return friendlyName;
     }
   }
+
   default double getPosition() {
-    return 0.0d; //TODO [IMPORTANT] Write this method
+    return 0.0d; // TODO [IMPORTANT] Write this method
   }
 }
