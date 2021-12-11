561dfaa97ea522127ae28e68e29a6cc3f8a7881b
KiARC <katiearose@protonmail.com>
2021-12-11 12:17:38 -0500

    Add ToDos

--- src/main/java/frc/team449/javaMaps/builders/SmartMotorConfig.java
+++ src/main/java/frc/team449/javaMaps/builders/SmartMotorConfig.java
@@ -26,7 +26,7 @@ public class SmartMotorConfig {
   private boolean enableBrakeMode;
   private @Nullable String name;
   private boolean reverseOutput;
-  private @Nullable PDP pdp;
+  private @Nullable PDP pdp; //TODO [IMPORTANT] Find a way to make this @NotNull. Current band-aid fix (@Nullable) is terrible.
   private @Nullable Boolean fwdLimitSwitchNormallyOpen;
   private @Nullable Boolean revLimitSwitchNormallyOpen;
   private @Nullable Integer remoteLimitSwitchID;
@@ -253,7 +253,7 @@ public class SmartMotorConfig {
   }
 
   /** Ensure that all required fields of this {@link SmartMotorConfig} have been initialized */
-  public SmartMotorConfig ensureBuilt() { // TODO We probably don't need this
+  public SmartMotorConfig ensureBuilt() { // TODO [IMPORTANT] We should remove this
     assert pdp != null : "PDP was null when constructing motor " + name;
     return this;
   }
